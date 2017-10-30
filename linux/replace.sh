05d0f86f5602d4061f01ecb85796ef6403e38e65
shangwenhe <shangwenhe@users.noreply.github.com>
2017-10-30 02:15:42 -0500

    Update replace.sh

--- linux/replace.sh
+++ linux/replace.sh
@@ -3,4 +3,6 @@
 
 tar zxvf ./output.tar.gz
 
-find ./output  -name '*'  -exec sed  -i '' 's/www.test.com.cn/dev-01.test.com.cn/g;s/shang/test/g;s/index.js/entry.js/g' {} \;
+# 对查找到的文件进行字符串替换
+find ./output  -name '*'  -exec sed  -i '' 's/https:\/\/www.test.com.cn/https:\/\/dev-01.test.com.cn/g;s/shang/test/g;s/index.js/entry.js/g' {} \;
+find ./output  -name '*'  -exec sed  -i '' 's/http\:\/\/www.test.com.cn/http\:\/\/dev-01.test.com.cn/g;s/shang/test/g;s/index.js/entry.js/g' {} \;
